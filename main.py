2aee32ecbf90ddc852d9961dc24e175ec887c79b
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-07-28 17:11:19 +0500

    almost comleted

--- main.py
+++ main.py
@@ -4,10 +4,27 @@ from markdown_pdf import MarkdownPdf, Section
 from io import BytesIO
 import firebase_admin
 from firebase_admin import credentials, firestore
+import yaml
+from yaml.loader import SafeLoader
 import streamlit_authenticator as stauth
+import streamlit_analytics
+from streamlit_authenticator.utilities import (CredentialsError,
+                                               ForgotError,
+                                               Hasher,
+                                               LoginError,
+                                               RegisterError,
+                                               ResetError,
+                                               UpdateError)
+
+
+# Loading config file
+with open('./users.yaml', 'r', encoding='utf-8') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+Hasher.hash_passwords(config['credentials'])
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("ai-roadmap-generator-f6570-firebase-adminsdk-kaovf-55e381062a.json")
+    cred = credentials.Certificate("ai-roadmap-generator-f6570-firebase-adminsdk-kaovf-292c7ace63.json")
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
@@ -17,25 +34,17 @@ client = Groq(
     api_key=st.secrets["GROQ_API_KEY"],
 )
 
-hashed_passwords = stauth.Hasher.hash_passwords({
-            "usernames": {
-                "admin": {
-                    "name": "Admin User",
-                    "password": "admin",
-                    "email": "admin@example.com"
-                }
-            },
-            "cookie": {
-                "expiry_days": 1,
-                "key": "asdfjksdf"
-            },
-            "preauthorized": {
-                "emails": ["admin@example.com"]
-            }
-})
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
+
+streamlit_analytics.start_tracking()
 
 
-authenticator = stauth.Authenticate(hashed_passwords, "aksjdfl;", "aksjdfkla")
 
 def generate_roadmap(user_input):
     prompt = f"Generate a comprehensive roadmap for learning {user_input['skill']} in {user_input['duration']} months. Divide the topics in weeks. Make sure you include projects."
@@ -56,8 +65,6 @@ def save_to_pdf(content):
 
     pdf.save("roadmap.pdf")
 
-
-    # Read the file into a BytesIO buffer
     buffer = BytesIO()
     with open("roadmap.pdf", "rb") as f:
         buffer.write(f.read())
@@ -102,17 +109,7 @@ if authentication_status:
         roadmap = generate_roadmap(user_input)
         # st.markdown(roadmap)
 
-        # Save the roadmap to PDF
         pdf_buffer = save_to_pdf(roadmap)
-
-        st.download_button(
-            label="Download Roadmap as PDF",
-            data=pdf_buffer,
-            file_name=f"{skill}_roadmap.pdf",
-            mime="application/pdf"
-        )
-
-        # Save roadmap to Firestore
         save_roadmap_to_db(username, skill, roadmap)
 
     roadmaps = load_roadmaps(username)
@@ -141,3 +138,21 @@ elif authentication_status == False:
 
 elif authentication_status == None:
     st.warning("Please enter your username and password")
+
+if not authentication_status:
+    # Creating a new user registration widget
+    try:
+        (email_of_registered_user,
+        username_of_registered_user,
+        name_of_registered_user) = authenticator.register_user(pre_authorization=False)
+        if email_of_registered_user:
+            st.success('User registered successfully')
+    except RegisterError as e:
+        st.error(e)
+
+
+# Saving config file
+with open('./users.yaml', 'w', encoding='utf-8') as file:
+    yaml.dump(config, file, default_flow_style=False)
+
+streamlit_analytics.stop_tracking()
